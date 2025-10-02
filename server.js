50bf83eb49fdb49d6bf4472ab5cdadd596751273
KristinaKolesnyk <kkristina.kolesnyk@gmail.com>
2025-10-02 14:02:24 -0400

    /register connect to frontend

--- server.js
+++ server.js
@@ -34,20 +34,19 @@ app.get('/', (req, res) => {
 
 app.post('/signin', (req, res) => {
     if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json('success');
+        res.json(database.users[0]);
     } else {
         res.status(400).json('error login in')
     }
 })
 
 app.post('/register', (req, res) => {
-    const {email, name, password} = req.body;
+    const {email, name} = req.body;
 
     database.users.push({
         id: '125',
         name: name,
         email: email,
-        password: password,
         entries: 0,
         joined: new Date(),
     });
